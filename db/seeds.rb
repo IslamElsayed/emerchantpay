aa687a8033fe4d061d95e90281776f6639f3325a
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2022-11-18 21:23:46 +0200

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Admin.create(email: 'admin@email.com', name: 'admin', password: 123456)
+
+3.times do |i|
+  merchant = Merchant.create(email: "merchant#{i}@email.com", name: "merchant#{i}", password: 123456)
+  %w(AuthorizeTransaction ChargeTransaction RefundTransaction ReversalTransaction).each do |transaction_type|
+    merchant.transactions.create!(customer_email: 'customer@email.com', customer_phone: 123456789, uuid: "uuid#{merchant.id}",
+                                  type: transaction_type, amount: 10)
+  end
+end
