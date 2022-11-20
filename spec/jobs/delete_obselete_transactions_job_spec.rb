1f6faf2ed8ad321c05bfc6ba398307ddc2cafe9d
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2022-11-20 21:40:34 +0200

    enhancce spec

--- spec/jobs/delete_obselete_transactions_job_spec.rb
+++ spec/jobs/delete_obselete_transactions_job_spec.rb
@@ -4,10 +4,21 @@ require 'rails_helper'
 
 RSpec.describe DeleteObseleteTransactionsJob, type: :job do
   describe '#perform_sync' do
-    before { create_list(:transaction, 5, type: 'AuthorizeTransaction' , created_at: 2.hours.ago) }
+    context 'with records older than 1 hour' do
+      before { create_list(:transaction, 5, type: 'AuthorizeTransaction',
+                                            created_at: 2.hours.ago) }
 
-    it 'deletes transactions created more than one hour ago' do
-      expect { DeleteObseleteTransactionsJob.perform_sync }.to change { Transaction.count }.by(-5)
+      it 'deletes transactions created more than one hour ago' do
+        expect { DeleteObseleteTransactionsJob.perform_sync }.to change { Transaction.count }.by(-5)
+      end
+    end
+
+    context 'with records younger than 1 hour' do
+      before { create_list(:transaction, 5, type: 'AuthorizeTransaction') }
+
+      it 'deletes transactions created more than one hour ago' do
+        expect { DeleteObseleteTransactionsJob.perform_sync }.not_to change { Transaction.count }
+      end
     end
   end
 end
